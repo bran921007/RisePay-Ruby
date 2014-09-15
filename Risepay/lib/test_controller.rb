3e6358893ccdbb72716ffb56f3273f72e4e303ef
risepay <jamie@risepayments.com>
2014-09-15 02:23:55 -0400

    test, indentation, update rubygems

--- Risepay/lib/test_controller.rb
+++ Risepay/lib/test_controller.rb
@@ -1,41 +1,77 @@
 require_relative 'risepays.rb'
 
-
 class TestController
-  def index
-  	data ={}
-    
 
-    data['NameOnCard']= "Jhonny";
-    data['CardNum']="5149612222222229";
-    data['ExpDate']="1214";
-    data['Amount']="22";
-    data['CVNum']="678";
-    data['InvNum']="ABC123";
-    data['Zip']="33139";
-    data['Street']="Gran vio 25";
-    data['TipAmt']=1;
+  attr_accessor :risepay, :data, :result, :msg
+
+  def initialize
+    @data ={}
+    
+    @data['NameOnCard']= "Jhonny";
+    @data['CardNum']="5149612222222229";
+    @data['ExpDate']="1214";
+    @data['Amount']="22";
+    @data['CVNum']="678";
 
     @risepay = Risepays.new("demo","demo")
 
-
-    @result =  @risepay.sale(data)
-
-
-    def result_msg
-      @message  
-      if @result['Approved']
-       @message = "Approved. Transaction ID = " + @result['PNRef'] +  "\n"  +    "AuthCode = " + @result['AuthCode']
-      else
-       @message =  "Declined " + @result['Message']
-
-      end	
-      return @message
-
-    end
-
-   @msg = result_msg()
+    @result =''
+    @msg = ''
+    #Call your transaction type
+    Sale(@data)
 
   end
 
+  def result_msg
+    @message
+    if @result['Approved']
+      @message = "Approved. Transaction ID = " + @result['PNRef'] +  "\n"  +    "AuthCode = " + @result['AuthCode']
+
+    else
+      @message =  "Declined " + @result['Message']
+
+    end 
+
+    return @message
+  end
+
+
+  def Sale(data)
+
+    @result =  @risepay.sale(data)
+
+    result_msg()
+
+  end
+
+  def Auth(data)
+
+    @result =  @risepay.auth(data)
+
+    result_msg()
+  end  
+
+  def Void(data)
+    data["PNRef"] = "24323401"
+    @result =  @risepay.void(data)
+
+    result_msg()
+  end  
+
+  def Return(data)
+
+    data["PNRef"] = "24323401"
+    @result =  @risepay.returnTrans(data)
+
+    result_msg()
+  end  
+
+  def Capture(data)
+    data["PNRef"] = "24323401"
+    @result =  @risepay.capture(data)
+
+    result_msg()
+  end
+
 end
+
